2f32f73d679d6301986816946623e1aab9f398c4
mradhyaf <mradhyaf@gmail.com>
2024-03-28 04:58:17 +0800

    Whitespace linting

--- tests/normalization/test_normal_form.py
+++ tests/normalization/test_normal_form.py
@@ -7,10 +7,9 @@ class TestBCNFCheck(unittest.TestCase):
     def setUp(self):
         # Duplicate; will be moved later on a data file
         self.fd = [(BinaryWord(5, 3), BinaryWord(5, 28)), (BinaryWord(5, 5), BinaryWord(5, 26)),
-                     (BinaryWord(5, 2), BinaryWord(5, 4)), (BinaryWord(5, 4), BinaryWord(5, 2)),
-                     (BinaryWord(5, 4), BinaryWord(5, 8)), (BinaryWord(5, 2), BinaryWord(5, 16)),
-                     (BinaryWord(5, 4), BinaryWord(5, 16))]
-
+                   (BinaryWord(5, 2), BinaryWord(5, 4)), (BinaryWord(5, 4), BinaryWord(5, 2)),
+                   (BinaryWord(5, 4), BinaryWord(5, 8)), (BinaryWord(5, 2), BinaryWord(5, 16)),
+                   (BinaryWord(5, 4), BinaryWord(5, 16))]
 
         # AB -> CDE, AC -> BDE, BC -> C
         self.fd_bcnf = [(BinaryWord(5, 3), BinaryWord(5, 28)), (BinaryWord(5, 5), BinaryWord(5, 26)),
@@ -38,6 +37,7 @@ class Test3NFCheck(unittest.TestCase):
         # AB -> CDE, AC -> BDE, BC -> C
         self.fd_bcnf = [(BinaryWord(5, 3), BinaryWord(5, 28)), (BinaryWord(5, 5), BinaryWord(5, 26)),
                         (BinaryWord(5, 6), BinaryWord(5, 4))]
+
     def test_is_3nf(self):
         self.assertFalse(
             is_3nf(self.fd_2nf_not_3nf)
@@ -46,6 +46,7 @@ class Test3NFCheck(unittest.TestCase):
         self.assertTrue(
             is_3nf(self.fd_bcnf)
         )
+
 
 if __name__ == '__main__':
     unittest.main()
